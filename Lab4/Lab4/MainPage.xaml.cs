f92ac6764e802510f4824efc8a6388c9065403d5
Elard <elard.sd.9@gmail.com>
2021-03-29 21:49:35 -0500

    Lab4 Terminado

---
+++ Lab4/Lab4/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Page1());
+            };
+
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new TabbedPage1());
+            };
+
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new CarouselPage1());
+            };
+
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushModalAsync(new ModalPage1());
+            };
+
+            Item5.Clicked += async (sender, e) =>
+            {
+                var answer = await DisplayAlert("¿Pregunta?", "¿Te gusto el juego?", "Si", "No");
+                System.Diagnostics.Debug.WriteLine("Answer: "+answer);
+            };
+        }
+    }
+}
