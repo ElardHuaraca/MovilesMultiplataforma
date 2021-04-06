bc835289c2857f4ad9e7850d10b7b01b70d4f190
unknown <elard.sd.9@gmail.com>
2021-04-05 22:20:58 -0500

    Lab Semana 5 terminado

---
+++ Lab5/Lab5/ImagenResourceExtencion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Reflection;
+
+namespace Lab5
+{
+    [Preserve(AllMembers = true)]
+    [ContentProperty(nameof(Source))]
+    class ImagenResourceExtencion : IMarkupExtension
+    {
+        public String Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider) 
+        {
+            if (Source == null) {
+                return null;
+            }
+
+            var imageSource = ImageSource.FromResource(Source,
+                typeof(ImagenResourceExtencion).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+}
